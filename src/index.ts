af3594c0db43be3ad0d1dd6b4ef97816771a4090
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2023-02-20 22:32:51 +0100

    feat: render

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,34 @@
 import {createQwikCity} from '@builder.io/qwik-city/middleware/node'
+import {ServerRenderOptions} from '@builder.io/qwik-city/middleware/request-handler'
+import fastifyStatic from '@fastify/static'
+import qwikCityPlan from '@qwik-city-plan'
 import type {FastifyPluginAsync} from 'fastify'
 import fastifyPlugin from 'fastify-plugin'
 
 interface QwikPluginOptions {
     distDir: string
     buildDir: string
+    render: ServerRenderOptions['render']
 }
 
 const qwikPlugin: FastifyPluginAsync<QwikPluginOptions> = async (fastify, options) => {
+    const {render, buildDir, distDir} = options
+
     const {router, notFound} = createQwikCity({render, qwikCityPlan})
 
+    fastify.register(fastifyStatic, {
+        root: buildDir,
+        prefix: '/build',
+        immutable: true,
+        maxAge: '1y',
+        decorateReply: false
+    })
 
+    fastify.register(fastifyStatic, {
+        root: distDir,
+        redirect: false,
+        decorateReply: false
+    })
 
 }
 
